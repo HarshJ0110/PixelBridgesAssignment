dcf58736527a0ab8dbbd8cb6e34002c93e0c6ac8
Harsh Jain <harshjain0461@gmail.com>
2023-12-27 20:26:52 +0530

    Update Modal.js

--- src/Components/Liquidity/LiquidityCard/Modal.js
+++ src/Components/Liquidity/LiquidityCard/Modal.js
@@ -30,13 +30,13 @@ const Modal = (props) => {
 
     return (
         <div className='absolute z-20 w-[50vw] top-0 right-0 h-screen md:w-[30vw] bg-black bg-opacity-100 translate-x-0 transition-all'>
-            <button className='text-gray-300 m-5 mx-10' onClick={handleClick}><i class="fa-regular fa-circle-xmark" onClick={handleClick}></i>
+            <button className='text-gray-300 m-5 mx-10' onClick={handleClick}><i class="fa-regular fa-circle-xmark text-2xl" onClick={handleClick}></i>
             </button>
-            <p className='text-gray-300 m-5 mx-10 hover:text-blue-500' onClick={() => { setName("USDT");setclick(false) }}>USDT</p>
-            <p className='text-gray-300 m-5 mx-10 hover:text-blue-500' onClick={() => { setName("BNB");setclick(false)  }}>BNB</p>
-            <p className='text-gray-300 m-5 mx-10 hover:text-blue-500' onClick={() => { setName("BTC") ;setclick(false) }}>BTC</p>
-            <p className='text-gray-300 m-5 mx-10 hover:text-blue-500' onClick={() => { setName("SOL") ;setclick(false) }}>SOL</p>
-            <p className='text-gray-300 m-5 mx-10 hover:text-blue-500' onClick={() => { setName("LTC");setclick(false)  }}>LTC</p>
+            <p className='text-gray-300 m-5 mx-10 font-medium text-xl hover:text-blue-500' onClick={() => { setName("USDT");setclick(false) }}>USDT</p>
+            <p className='text-gray-300 m-5 mx-10 font-medium text-xl hover:text-blue-500' onClick={() => { setName("BNB");setclick(false)  }}>BNB</p>
+            <p className='text-gray-300 m-5 mx-10 font-medium text-xl hover:text-blue-500' onClick={() => { setName("BTC") ;setclick(false) }}>BTC</p>
+            <p className='text-gray-300 m-5 mx-10 font-medium text-xl hover:text-blue-500' onClick={() => { setName("SOL") ;setclick(false) }}>SOL</p>
+            <p className='text-gray-300 m-5 mx-10 font-medium text-xl hover:text-blue-500' onClick={() => { setName("LTC");setclick(false)  }}>LTC</p>
         </div>
     )
 }
